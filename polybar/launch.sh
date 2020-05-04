6b5dac854b733b7819b56d55786c1804a1077ba1
Jess Totty <jessicahtotty@gmail.com>
2020-05-04 11:31:42 -0500

    (sorta) cleaned up bar logic - needs more work, so added TODO

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,17 +8,18 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+#TODO clean up bars
 # Launch polybar for the main display
-polybar bar-primary &
+#polybar bar-primary &
 
 # Launch polybar for the two other monitors, if they are active
 AVAILABLE_MONITORS=$(polybar --list-monitors)
-if echo $AVAILABLE_MONITORS | grep -q 'eDP1'; then
-	polybar bar-left &
+if echo $AVAILABLE_MONITORS | grep -q 'eDP-1'; then
+	polybar bar-primary &
 fi
-#if echo $AVAILABLE_MONITORS | grep -q 'HDMI1'; then
-#	polybar bar-single-monitor &
-#fi
+if echo $AVAILABLE_MONITORS | grep -q 'HDMI-1'; then
+	polybar bar-single-monitor &
+fi
 if echo $AVAILABLE_MONITORS | grep -q 'DisplayPort-1-2'; then
 	polybar bar-right &
 fi

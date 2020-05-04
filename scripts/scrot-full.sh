ae02fc2a313582adffd3efb71fd9d7df2bc57bfd
Jess Totty <jessicahtotty@gmail.com>
2020-05-04 15:03:54 -0500

    changed path from pics to images. honestly can't remember why...

--- scripts/scrot-full.sh
+++ scripts/scrot-full.sh
@@ -2,10 +2,10 @@
 # ~/.config/scripts/scrot-full.sh
 # Script to take a screenshot (fullscreen)
 
-if [ ! -d "$HOME/pics/screenshots" ]; then
-    mkdir -p "$HOME/pics/screenshots"
+if [ ! -d "$HOME/images/screenshots" ]; then
+    mkdir -p "$HOME/images/screenshots"
 fi
 
 notify-send -u low "i3" "Taking screenshot..." &
-scrot --delay 3 --silent "$HOME/pics/screenshots/screenshot.%y-%m-%d.%H-%M.png"
+scrot --delay 3 --silent "$HOME/images/screenshots/screenshot.%y-%m-%d.%H-%M.png"
 notify-send -u low "i3" "Saved screenshot." &

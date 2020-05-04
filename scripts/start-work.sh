576e25ebabee2255cf0f65a34363d44dc80de750
Jess Totty <jessicahtotty@gmail.com>
2020-05-04 11:36:21 -0500

    remove super old/outdated work script

--- scripts/start-work.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# ~/.config/scripts/start-work.sh
-
-i3-msg 'workspace 2; exec /usr/bin/chromium --app=https://rocketchat.wolfram.com' && sleep 5s
-i3-msg 'workspace 7; exec /usr/bin/thunderbird'

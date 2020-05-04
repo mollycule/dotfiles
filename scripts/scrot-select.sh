f2a6fd0897837852592f83248b413fec951280cf
Jess Totty <jessicahtotty@gmail.com>
2020-05-04 15:06:23 -0500

    changed path from pics to images for some reason

--- scripts/scrot-select.sh
+++ scripts/scrot-select.sh
@@ -2,10 +2,10 @@
 # ~/.config/scripts/scrot-select.sh
 # Script to take a screenshot (selection)
 
-if [ ! -d "$HOME/pics/screenshots" ]; then
-    mkdir -p "$HOME/pics/screenshots"
+if [ ! -d "$HOME/images/screenshots" ]; then
+    mkdir -p "$HOME/images/screenshots"
 fi
 
 notify-send -u low "i3" "Select an area to capture screenshot..." -t 2000 &
-scrot --select --silent "$HOME/pics/screenshots/screenshot.%y-%m-%d.%H-%M.png"
+scrot --select --silent "$HOME/images/screenshots/screenshot.%y-%m-%d.%H-%M.png"
 notify-send -u low "i3" "Saved screenshot." &

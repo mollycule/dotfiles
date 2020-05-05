ad5b814e321d5b525351a3081064a2ac4291e36f
Jess Totty <jessicahtotty@gmail.com>
2020-05-05 16:27:49 -0500

    add exports.sh

---
+++ x/exports.sh
@@ -0,0 +1,8 @@
+# Contains exported environment variables.
+export DUNST_FONT='Iosevka SS02 10'
+export DUNST_SIZE='300x30+2414+20'
+export EDITOR='vim'
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US.UTF-8'
+#export LC_CTYPE='en_US.UTF-8' can't set for some reason (no file or dir) 12292019
+export UI_FONT='Terminus 12'

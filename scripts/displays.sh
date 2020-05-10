1e46f75a40e0fc2d639f298c1e40268f1012f132
Jess Totty <jessicahtotty@gmail.com>
2020-05-10 15:22:15 -0500

    remove work layout, move xrandr commands to separate files

--- scripts/displays.sh
+++ scripts/displays.sh
@@ -4,16 +4,12 @@
 
 display_count=$(xrandr | grep ' connected' | wc -l)
 
-work_docked='xrandr --output VIRTUAL1 --off --output DisplayPort-1-2 --mode 1920x1080 --pos 3520x120 --rotate normal --output eDP1 --primary --mode 1600x900 --pos 0x88 --rotate normal --output DP1 --off --output DisplayPort-1-3 --off --output HDMI2 --off --output HDMI1 --mode 1920x1200 --pos 1600x0 --rotate normal --output DP2 --off'
+one_monitor="$HOME/.screenlayout/monitor_4k.sh"
 
-undocked='xrandr --output VIRTUAL1 --off --output DisplayPort-1-2 --off --output eDP1 --primary --mode 1600x900 --pos 1920x180 --rotate normal --output DP1 --off --output DisplayPort-1-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off'
+undocked="$HOME/.screenlayout/laptop_1080p.sh"
 
-one_monitor_left='xrandr --output VIRTUAL1 --off --output DisplayPort-1-2 --off --output eDP1 --primary --mode 1600x900 --pos 1280x124 --rotate normal --output DP1 --off --output DisplayPort-1-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --mode 1280x1024 --pos 0x0 --rotate normal'
-
-if [ $display_count -eq 3 ]; then
-    $work_docked
-elif [ $display_count -eq 2 ]; then
-    $one_monitor_left
+if [ $display_count -eq 2 ]; then
+    $one_monitor
 else
     $undocked
 fi

04c9e73089684aaebabdd2a109d5ddb8a1c04704
Jess Totty <jessicahtotty@gmail.com>
2020-05-12 18:54:20 -0500

    make find command follow symlinks since xrandr configs are in .config now

--- scripts/rofi-display.sh
+++ scripts/rofi-display.sh
@@ -2,7 +2,7 @@
 # ~/.config/scripts/rofi-display.sh
 # Rofi wrapper around xrandr screenlayouts
 
-layouts="$(find $HOME/.screenlayout -maxdepth 1 -executable -type f | cut -d '/' -f 5 | sort | paste -sd '|')"
+layouts="$(find -L $HOME/.screenlayout -maxdepth 1 -executable -type f | cut -d '/' -f 5 | sort | paste -sd '|')"
 prompt="Switch X RandR Layout :"
 choice=$(echo "CANCEL|$layouts" | rofi -sep '|' -dmenu -i -only-match -p "$prompt")
 

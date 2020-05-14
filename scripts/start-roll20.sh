80a39d4b8739a66c27690fbf4f379b8b480336f0
Jess Totty <jessicahtotty@gmail.com>
2020-05-14 16:15:35 -0500

    add dm guide and update emacs todo

--- scripts/start-roll20.sh
+++ scripts/start-roll20.sh
@@ -6,8 +6,9 @@
 # Use Chromium until Firefox fixes rendering issue
 i3-msg 'workspace 1; exec /usr/bin/chromium "https://roll20.net/welcome"' && sleep 2s
 i3-msg 'workspace 1; exec /usr/bin/discord' && sleep 3s
-# TODO open saved session of campaign org docs
+# TODO open saved session of campaign org docs - saved workspace to file, not sure if it can be defined on cmdline
 i3-msg 'workspace 1; exec /usr/bin/emacs' && sleep 2s
 i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/player-handbook.pdf"'
 i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/sword-coast-guide.pdf"'
+i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/dungeon-master-guide.pdf"'
 i3-msg 'workspace 2; exec /usr/bin/slack'

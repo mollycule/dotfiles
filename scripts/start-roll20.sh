d0245da9fc1cc0d15b0df980660693c198240382
Jess Totty <jessicahtotty@gmail.com>
2020-05-26 20:28:24 -0500

    add image of character sheet skills and unfortunately more sleep statements

--- scripts/start-roll20.sh
+++ scripts/start-roll20.sh
@@ -5,11 +5,13 @@
 # TODO get an i3 layout defined so that i don't use sleep statements to align things
 # Use Chromium until Firefox fixes rendering issue
 i3-msg 'workspace 1; exec /usr/bin/chromium "https://roll20.net/welcome"' && sleep 2s
-i3-msg 'workspace 1; exec /usr/bin/discord' && sleep 3s
+# TODO figure out why discord only opens on active workspace, not what's specified
+i3-msg 'workspace 1; exec /usr/bin/discord' && sleep 2s
 # TODO open saved session of campaign org docs - saved workspace to file, not sure if it can be defined on cmdline
-i3-msg 'workspace 1; exec /usr/bin/emacs' && sleep 2s
-i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/player-handbook.pdf"'
-i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/sword-coast-guide.pdf"'
-i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/dungeon-master-guide.pdf"'
-i3-msg 'workspace 2; exec /usr/bin/slack'
-i3-msg 'workspace 3; exec /usr/bin/firefox "file://$HOME/personal/dnd/dungeon-master-screen.pdf"'
+i3-msg 'workspace 1; exec /usr/bin/emacs' && sleep 1s
+i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/player-handbook.pdf"' && sleep 1s
+i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/sword-coast-guide.pdf"' && sleep 1s
+i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/dungeon-master-guide.pdf"' && sleep 1s
+i3-msg 'workspace 2; exec /usr/bin/feh --image-bg black $HOME/personal/dnd/character-sheet-skills.png'
+i3-msg 'workspace 2; exec /usr/bin/slack' && sleep 2s
+i3-msg 'workspace 3; exec /usr/bin/firefox --new-window "file://$HOME/personal/dnd/dungeon-master-screen.pdf"'

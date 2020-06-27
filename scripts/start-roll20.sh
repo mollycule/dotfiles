69317948057ecfe73b571e70babcd8552deae694
Jess Totty <jessicahtotty@gmail.com>
2020-06-27 13:37:30 -0500

    shift things around, moved chromium to firefox because rendering issues have been fixed (yay)

--- scripts/start-roll20.sh
+++ scripts/start-roll20.sh
@@ -3,12 +3,11 @@
 # Script to automate DMing in Roll20
 
 # TODO get an i3 layout defined so that i don't use sleep statements to align things
-# Use Chromium until Firefox fixes rendering issue
-i3-msg 'workspace 1; exec /usr/bin/chromium "https://roll20.net/welcome"' && sleep 2s
-# TODO figure out why discord only opens on active workspace, not what's specified
-i3-msg 'workspace 1; exec /usr/bin/discord' && sleep 2s
 # TODO open saved session of campaign org docs - saved workspace to file, not sure if it can be defined on cmdline
 i3-msg 'workspace 1; exec /usr/bin/emacs' && sleep 1s
+# TODO figure out why discord only opens on active workspace, not what's specified
+i3-msg 'workspace 1; exec /usr/bin/discord' && sleep 2s
+i3-msg 'workspace 1; exec /usr/bin/firefox --new-window "https://roll20.net/welcome"' && sleep 2s
 i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/player-handbook.pdf"' && sleep 1s
 i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/sword-coast-guide.pdf"' && sleep 1s
 i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/dungeon-master-guide.pdf"' && sleep 1s

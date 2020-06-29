f18462971359dbeaf487520d3ba7eb291cee598e
Jess Totty <jessicahtotty@gmail.com>
2020-06-29 16:09:19 -0500

    remove duplicate lines already in wal-set.sh, and add wal-set.sh

--- scripts/i3-restart.sh
+++ scripts/i3-restart.sh
@@ -2,12 +2,8 @@
 # ~/.config/scripts/i3-restart.sh
 # Wrapper script for restarting i3 in-place
 
-if [ -f "$HOME/.cache/wal/colors.sh" ]; then
-    source "$HOME/.cache/wal/colors.sh"
-fi
-
 rofi -e "[!] The window manager will now restart." -width -44
-killall -q polybar >/dev/null 2>&1
 i3-msg reload >/dev/null 2>&1
-$HOME/.config/polybar/launch.sh
+$HOME/.config/scripts/wal-set.sh # reload dunst, polybar, and wal
 notify-send -u low "i3" "Successfully restarted window manager..." &
+

6897af06731baae5242aa050a9d15016d0bf82fe
Jess Totty <jessicahtotty@gmail.com>
2020-07-09 13:02:37 -0500

    force new firefox window of dnd books

--- scripts/start-roll20.sh
+++ scripts/start-roll20.sh
@@ -8,7 +8,7 @@ i3-msg 'workspace 1; exec /usr/bin/emacs' && sleep 1s
 # TODO figure out why discord only opens on active workspace, not what's specified
 i3-msg 'workspace 1; exec /usr/bin/discord' && sleep 2s
 i3-msg 'workspace 1; exec /usr/bin/firefox --new-window "https://roll20.net/welcome"' && sleep 2s
-i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/player-handbook.pdf"' && sleep 1s
+i3-msg 'workspace 2; exec /usr/bin/firefox --new-window "file://$HOME/personal/dnd/player-handbook.pdf"' && sleep 1s
 i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/sword-coast-guide.pdf"' && sleep 1s
 i3-msg 'workspace 2; exec /usr/bin/firefox "file://$HOME/personal/dnd/dungeon-master-guide.pdf"' && sleep 1s
 i3-msg 'workspace 2; exec /usr/bin/feh --auto-zoom --image-bg black $HOME/personal/dnd/character-sheet-skills.png'

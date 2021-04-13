4f58c6dc1645457b40c8515c93614e81c824e9d2
Jess Totty <jessicahtotty@gmail.com>
2021-04-13 18:45:24 -0500

    add --auto so monitor is noticed on bootup

--- screenlayout/monitor_4k_DP.sh
+++ screenlayout/monitor_4k_DP.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-2-1 --off --output DP-2-2 --primary --mode 5120x1440 --pos 0x0 --rotate normal --output DP-2-3 --off
+xrandr --output DP-1-2 --auto --primary --mode 5120x1440 --pos 0x0 --rotate normal --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-2-1 --off --output DP-2-3 --off

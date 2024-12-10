b646ba8cc5b28465e60e6a153369fdac06690dfc
jhaney <jhaney@illinois.edu>
2024-12-10 12:51:47 -0600

    add laptop otion, change spacing 4->2, use grep -c

--- scripts/displays.sh
+++ scripts/displays.sh
@@ -2,14 +2,16 @@
 # ~/.config/scripts/displays.sh
 # A script to appropriately configure xrandr
 
-display_count=$(xrandr | grep ' connected' | wc -l)
+display_count=$(xrandr | grep -c ' connected')
 
 one_monitor="$HOME/.screenlayout/monitor_4k_DP.sh"
-
+two_monitor="$HOME/.screenlayout/work_laptop.sh"
 undocked="$HOME/.screenlayout/laptop_1080p.sh"
 
-if [ $display_count -eq 2 ]; then
-    $one_monitor
+if [ "$display_count" -eq 2 ]; then
+  $one_monitor
+elif [ "$display_count" -eq 3 ]; then
+ $two_monitor
 else
-    $undocked
+  $undocked
 fi
